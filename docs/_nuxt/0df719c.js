c3d84cc26e147aeaab2af3bf7532677104bbcd72
QWERx29 <fengjiuheng@163.com>
2022-08-29 14:39:51 +0800

    QAQ

---
+++ docs/_nuxt/0df719c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{255:function(n,e,t){"use strict";t.r(e);var l={name:"IndexPage"},o=t(57),component=Object(o.a)(l,(function(){return(0,this._self._c)("h2",[this._v("This is a demo page by Phantasus_")])}),[],!1,null,null,null);e.default=component.exports}}]);
